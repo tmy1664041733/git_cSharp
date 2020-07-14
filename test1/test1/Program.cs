a0eccbe9fd3271d7a038646ea96030223938df6c
tmyanxing <1664041733@qq.com>
2020-07-14 21:21:47 +0800

    C sharp 程序 -if else 语句比较两个数的大小

---
+++ test1/test1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hollo World");
+            //Console.ReadKey();
+            //double firstNumber;
+            //string userName;
+            int a,b;
+            Console.WriteLine("输入一个数字a:");
+            a = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("输入一个数字b:");
+            b= Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("a大于b");
+            }else if(a<b)
+            {
+                Console.WriteLine("a小于b");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("a等于b");
+            }
+            Console.WriteLine("结果");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+            Console.ReadKey();
+
+            //Console.WriteLine("userName:");
+            //userName = Console.ReadLine();
+            //Console.WriteLine(userName);
+            //Console.ReadKey();
+        }
+    }
+}
