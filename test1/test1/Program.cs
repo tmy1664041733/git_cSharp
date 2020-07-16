3c201ea8de77cff44a315790c8d452b7a15e13da
tmyanxing <1664041733@qq.com>
2020-07-16 11:35:58 +0800

    类型转换以及枚举的使用

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace test1
 {
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
     class Program
     {
         static void Main(string[] args)
@@ -37,9 +44,25 @@ namespace test1
             //}
             //Console.ReadKey();
             //三目运算符
-            int var1 = 7;
-            int var2 = 10;
-           
+            //int var1 = 7;
+            //int var2 = 10;
+            double doubleVal = 99.999;
+            bool boolVal = true;
+            string stringResult;
+            //进行数据类型转换布尔类型和浮点型转化为字符型
+            stringResult = Convert.ToString(boolVal) + Convert.ToString(doubleVal);
+            Console.WriteLine("String:{0}+{1}={2}", boolVal, doubleVal, stringResult);
+            Console.ReadKey();
+            //枚举类型的使用
+            orientation myDirection = orientation.north;
+            Console.WriteLine("{0}", myDirection);
+            //进行类型转换才能显示枚举的字符值
+            string directionString;
+            byte directionByte;
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine("{0},{1}", directionString, directionByte);
+            Console.ReadKey();
           
         }
     }
